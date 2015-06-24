e7ece2332a9bb6a42ce061a20ed7eaef8a6c4e0b
Jeremy Cope <j.cope@me.com>
2015-06-23 22:25:28 -0400

    Fixed a compilation error- included wrong file. (Should have been checked prior to previous commit!)

--- tester/KpViewController.m
+++ tester/KpViewController.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "KpViewController.h"
 
 #import "KPAnnotation.h"
 #import "KPGridClusteringAlgorithm.h"
